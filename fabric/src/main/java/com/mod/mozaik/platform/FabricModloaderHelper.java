58b220674cd943481913978fa83a1482bd397e3b
Autumn <shinigami7x@proton.me>
2026-09-02 20:00:33 -0400

    Fixed key modifiers.
    (cherry picked from commit 232e59d4e8a380498a1a83cbca8a39828aa5b391)

--- fabric/src/main/java/com/mod/mozaik/platform/FabricModloaderHelper.java
+++ fabric/src/main/java/com/mod/mozaik/platform/FabricModloaderHelper.java
@@ -5,12 +5,15 @@ import com.mod.mozaik.blocks.MortarBlock;
 import com.mod.mozaik.blocks.entities.FabricMortarBlockEntity;
 import com.mod.mozaik.blocks.entities.MortarBlockEntity;
 import com.mod.mozaik.platform.services.IModloaderHelper;
+import com.mod.mozaik.util.IFabricKeyMapping;
+import com.mod.mozaik.util.IMozaikKeyMapping;
 import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import org.lwjgl.glfw.GLFW;
 
 public class FabricModloaderHelper implements IModloaderHelper {
 
@@ -26,12 +29,15 @@ public class FabricModloaderHelper implements IModloaderHelper {
 
     @Override
     public KeyMapping createKeyMapping(String name, InputConstants.Type type, int keyCode, int keyMod, KeyMapping.Category category) {
-        // TODO: [NYI]
-        return new KeyMapping(
-            name,
-            type,
-            keyCode,
-            category
+        KeyMapping mapping = new KeyMapping(name, type, keyCode, category);
+        ((IFabricKeyMapping) mapping).mozaik$setModifier(
+            switch (keyMod) {
+                case GLFW.GLFW_MOD_SHIFT -> IMozaikKeyMapping.Modifier.SHIFT;
+                case GLFW.GLFW_MOD_CONTROL -> IMozaikKeyMapping.Modifier.CONTROL;
+                case GLFW.GLFW_MOD_ALT -> IMozaikKeyMapping.Modifier.ALT;
+                default -> IMozaikKeyMapping.Modifier.NONE;
+            }
         );
+        return mapping;
     }
 }
---
+++ fabric/src/main/java/com/mod/mozaik/util/IFabricKeyMapping.java
@@ -0,0 +1,5 @@
+package com.mod.mozaik.util;
+
+public interface IFabricKeyMapping {
+    void mozaik$setModifier(IMozaikKeyMapping.Modifier modifier);
+}
