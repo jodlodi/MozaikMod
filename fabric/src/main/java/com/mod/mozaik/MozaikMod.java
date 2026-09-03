37f7de1538f9c6220779fb236c688e70d83cab5a
Autumn <shinigami7x@proton.me>
2026-09-02 20:25:42 -0400

    Ported events and did a bit of cleanup.

--- fabric/src/main/java/com/mod/mozaik/MozaikMod.java
+++ fabric/src/main/java/com/mod/mozaik/MozaikMod.java
@@ -1,15 +1,59 @@
 package com.mod.mozaik;
 
+import com.mod.mozaik.blocks.entities.MortarBlockEntity;
+import com.mod.mozaik.networking.bidirectional.AddPolyominoBidirectional;
+import com.mod.mozaik.networking.bidirectional.RemovePolyominoBidirectional;
+import com.mod.mozaik.networking.bidirectional.SignedMozaikBidirectional;
+import com.mod.mozaik.networking.bidirectional.UpdateMozaikBidirectional;
+import com.mod.mozaik.networking.clientbound.OpenGlueMenuClientbound;
+import com.mod.mozaik.networking.serverbound.SelectShardBagItemPacket;
 import com.mod.mozaik.platform.FabricNetworkHelper;
+import com.mod.mozaik.platform.Services;
+import com.mod.mozaik.util.FabricChunkWatchEvents;
 import com.mod.mozaik.util.FabricServerLifecycleHooks;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 
 public class MozaikMod implements ModInitializer {
     
     @Override
     public void onInitialize() {
         FabricServerLifecycleHooks.init();
+
         CommonClass.init(true);
-        FabricNetworkHelper.commonPacketRegistration();
+
+        registerCommonNetworking();
+        registerEvents();
+    }
+
+    private static void registerCommonNetworking() {
+        PayloadTypeRegistry.serverboundPlay().register(UpdateMozaikBidirectional.TYPE, UpdateMozaikBidirectional.STREAM_CODEC);
+        PayloadTypeRegistry.clientboundPlay().register(UpdateMozaikBidirectional.TYPE, UpdateMozaikBidirectional.STREAM_CODEC);
+        PayloadTypeRegistry.serverboundPlay().register(RemovePolyominoBidirectional.TYPE, RemovePolyominoBidirectional.STREAM_CODEC);
+        PayloadTypeRegistry.clientboundPlay().register(RemovePolyominoBidirectional.TYPE, RemovePolyominoBidirectional.STREAM_CODEC);
+        PayloadTypeRegistry.serverboundPlay().register(SignedMozaikBidirectional.TYPE, SignedMozaikBidirectional.STREAM_CODEC);
+        PayloadTypeRegistry.clientboundPlay().register(SignedMozaikBidirectional.TYPE, SignedMozaikBidirectional.STREAM_CODEC);
+        PayloadTypeRegistry.serverboundPlay().register(AddPolyominoBidirectional.TYPE, AddPolyominoBidirectional.STREAM_CODEC);
+        PayloadTypeRegistry.clientboundPlay().register(AddPolyominoBidirectional.TYPE, AddPolyominoBidirectional.STREAM_CODEC);
+
+        PayloadTypeRegistry.clientboundPlay().register(OpenGlueMenuClientbound.TYPE, OpenGlueMenuClientbound.STREAM_CODEC);
+        PayloadTypeRegistry.serverboundPlay().register(SelectShardBagItemPacket.TYPE, SelectShardBagItemPacket.STREAM_CODEC);
+
+        ServerPlayNetworking.registerGlobalReceiver(UpdateMozaikBidirectional.TYPE, FabricNetworkHelper::onServerMessage);
+        ServerPlayNetworking.registerGlobalReceiver(RemovePolyominoBidirectional.TYPE, FabricNetworkHelper::onServerMessage);
+        ServerPlayNetworking.registerGlobalReceiver(SignedMozaikBidirectional.TYPE, FabricNetworkHelper::onServerMessage);
+        ServerPlayNetworking.registerGlobalReceiver(AddPolyominoBidirectional.TYPE, FabricNetworkHelper::onServerMessage);
+        ServerPlayNetworking.registerGlobalReceiver(SelectShardBagItemPacket.TYPE, FabricNetworkHelper::onServerMessage);
+    }
+
+    private static void registerEvents() {
+        FabricChunkWatchEvents.SENT.register((entity, chunk, _) -> {
+            chunk.getBlockEntities().forEach((pos, blockEntity) -> {
+                if (blockEntity instanceof MortarBlockEntity mortarBlockEntity) {
+                    Services.NETWORK.sendToClient(entity, new UpdateMozaikBidirectional(mortarBlockEntity.getPolyomino(), pos));
+                }
+            });
+        });
     }
 }
--- fabric/src/main/java/com/mod/mozaik/platform/FabricNetworkHelper.java
+++ fabric/src/main/java/com/mod/mozaik/platform/FabricNetworkHelper.java
@@ -1,51 +1,17 @@
 package com.mod.mozaik.platform;
 
-import com.mod.mozaik.networking.bidirectional.AddPolyominoBidirectional;
-import com.mod.mozaik.networking.bidirectional.RemovePolyominoBidirectional;
-import com.mod.mozaik.networking.bidirectional.SignedMozaikBidirectional;
-import com.mod.mozaik.networking.bidirectional.UpdateMozaikBidirectional;
 import com.mod.mozaik.networking.clientbound.IClientboundMessage;
-import com.mod.mozaik.networking.clientbound.OpenGlueMenuClientbound;
 import com.mod.mozaik.networking.serverbound.IServerboundMessage;
-import com.mod.mozaik.networking.serverbound.SelectShardBagItemPacket;
 import com.mod.mozaik.platform.services.INetworkHelper;
 import com.mod.mozaik.util.FabricClientPacketDistributor;
 import com.mod.mozaik.util.FabricPacketDistributor;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.ChunkPos;
 
 public class FabricNetworkHelper implements INetworkHelper {
-	public static void commonPacketRegistration() {
-		PayloadTypeRegistry.serverboundPlay().register(UpdateMozaikBidirectional.TYPE, UpdateMozaikBidirectional.STREAM_CODEC);
-		PayloadTypeRegistry.clientboundPlay().register(UpdateMozaikBidirectional.TYPE, UpdateMozaikBidirectional.STREAM_CODEC);
-		PayloadTypeRegistry.serverboundPlay().register(RemovePolyominoBidirectional.TYPE, RemovePolyominoBidirectional.STREAM_CODEC);
-		PayloadTypeRegistry.clientboundPlay().register(RemovePolyominoBidirectional.TYPE, RemovePolyominoBidirectional.STREAM_CODEC);
-		PayloadTypeRegistry.serverboundPlay().register(SignedMozaikBidirectional.TYPE, SignedMozaikBidirectional.STREAM_CODEC);
-		PayloadTypeRegistry.clientboundPlay().register(SignedMozaikBidirectional.TYPE, SignedMozaikBidirectional.STREAM_CODEC);
-		PayloadTypeRegistry.serverboundPlay().register(AddPolyominoBidirectional.TYPE, AddPolyominoBidirectional.STREAM_CODEC);
-		PayloadTypeRegistry.clientboundPlay().register(AddPolyominoBidirectional.TYPE, AddPolyominoBidirectional.STREAM_CODEC);
-
-		PayloadTypeRegistry.clientboundPlay().register(OpenGlueMenuClientbound.TYPE, OpenGlueMenuClientbound.STREAM_CODEC);
-		PayloadTypeRegistry.serverboundPlay().register(SelectShardBagItemPacket.TYPE, SelectShardBagItemPacket.STREAM_CODEC);
-
-		ServerPlayNetworking.registerGlobalReceiver(UpdateMozaikBidirectional.TYPE, FabricNetworkHelper::onServerMessage);
-		ServerPlayNetworking.registerGlobalReceiver(RemovePolyominoBidirectional.TYPE, FabricNetworkHelper::onServerMessage);
-		ServerPlayNetworking.registerGlobalReceiver(SignedMozaikBidirectional.TYPE, FabricNetworkHelper::onServerMessage);
-		ServerPlayNetworking.registerGlobalReceiver(AddPolyominoBidirectional.TYPE, FabricNetworkHelper::onServerMessage);
-		ServerPlayNetworking.registerGlobalReceiver(SelectShardBagItemPacket.TYPE, FabricNetworkHelper::onServerMessage);
-	}
-
-	public static void clientPacketRegistration() {
-		ClientPlayNetworking.registerGlobalReceiver(UpdateMozaikBidirectional.TYPE, FabricNetworkHelper::onClientMessage);
-		ClientPlayNetworking.registerGlobalReceiver(RemovePolyominoBidirectional.TYPE, FabricNetworkHelper::onClientMessage);
-		ClientPlayNetworking.registerGlobalReceiver(SignedMozaikBidirectional.TYPE, FabricNetworkHelper::onClientMessage);
-		ClientPlayNetworking.registerGlobalReceiver(AddPolyominoBidirectional.TYPE, FabricNetworkHelper::onClientMessage);
-		ClientPlayNetworking.registerGlobalReceiver(OpenGlueMenuClientbound.TYPE, FabricNetworkHelper::onClientMessage);
-	}
 
 	@Override
 	public void sendToServer(IServerboundMessage payload, IServerboundMessage... payloads) {
